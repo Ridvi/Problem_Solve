0a2b469a4e538cccd5de0804ba496e8c74a4149a
Mahir Alvi Ridvi <69901682+Ridvi@users.noreply.github.com>
2025-07-31 21:57:41 +0600

    Update new_companies.sql

--- HackerRank/SQL/new_companies.sql
+++ HackerRank/SQL/new_companies.sql
@@ -1,1 +1,11 @@
-select name 
+SELECT c.company_code,c.founder,
+        count(distinct e.lead_manager_code),
+        count(distinct e.senior_manager_code),
+        count(distinct e.manager_code),
+        count(distinct e.employee_code)
+        
+FROM company as c
+join employee as e 
+using(company_code)
+group by c.company_code,c.founder
+order by company_code;
