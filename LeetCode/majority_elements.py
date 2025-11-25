06b94dff4334decf80da52186192630ec3993fba
Mahir Alvi Ridvi <69901682+Ridvi@users.noreply.github.com>
2025-11-25 19:40:45 +0600

    Easy

---
+++ LeetCode/majority_elements.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        freq=0
+        for i in nums:
+            if freq==0:
+                ans=i
+            if ans==i:
+                freq= freq+1
+            else:
+                freq = freq -1
+        return ans
